01d4527377707a5d2d62f6037dbfd839af10d572
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2022-09-17 00:21:35 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const refGallery = document.querySelector(".gallery");
+function createGallery() {
+  const markup = galleryItems
+    .map(
+      (item) => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    )
+    .join("");
+  refGallery.insertAdjacentHTML("beforeend", markup);
+}
+createGallery();
+refGallery.addEventListener("click", makeLightbox);
+function makeLightbox(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", { close: true });
+  document.addEventListener("keydown", closeLightbox);
+  function closeLightbox() {
+    lightbox.close();
+  }
+  // function onEscClose(event) {
+  //   if (event.code === "Escape") {
+  //     instance.close();
+  //     document.removeEventListener("keydown", onEscClose);
+  //   }
+  // }
+  // document.addEventListener("keydown", onEscClose);
+}
