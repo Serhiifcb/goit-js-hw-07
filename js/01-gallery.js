ffe8196d3b3ba5da984b146321910f73077959db
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2022-09-16 12:27:39 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const refGallery = document.querySelector(".gallery");
+function createGallery() {
+  const markup = galleryItems
+    .map(
+      (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+  refGallery.insertAdjacentHTML("beforeend", markup);
+}
+createGallery();
+refGallery.addEventListener("click", makeLightbox);
+function makeLightbox(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}">
+  `);
+  instance.show();
+  console.log(event.target.dataset.source);
+  document.addEventListener("keydown", (event) => {
+    console.log("code: ", event.code);
+    if (event.code === "Escape") {
+      return;
+    }
+  });
+}
 
-console.log(galleryItems);
+// console.log(galleryItems);
